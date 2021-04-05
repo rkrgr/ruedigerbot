c299c0daafd9db481edae693009fa21565f9ed6b
rkrgr <r.krgr@tuta.io>
2021-04-05 21:24:45 +0200

    add timeout parameter for sounds

--- src/soundplayer.js
+++ src/soundplayer.js
@@ -5,7 +5,8 @@ async function play(voiceChannel, soundNames) {
 		const connection = await voiceChannel.join();
 		const sounds = await s3.getSounds();
 		const legitSoundNames = soundNames.filter(soundName => {
-			const regex = new RegExp('\\/' + soundName + '\\.');
+			const namePart = soundName.split('(')[0];
+			const regex = new RegExp('\\/' + namePart + '\\.');
 			return sounds.find(sound => regex.test(sound.Key));
 		});
 		playNextSound(connection, legitSoundNames, sounds);
@@ -18,10 +19,24 @@ async function play(voiceChannel, soundNames) {
 function playNextSound(connection, soundNames, sounds) {
 	const nextSound = soundNames.shift();
 	if(nextSound) {
-		const regex = new RegExp('\\/' + nextSound + '\\.');
+		const soundSplit = nextSound.split('(');
+		const namePart = soundSplit[0];
+		let timePart;
+		if(soundSplit[1]) {
+			timePart = parseFloat(soundSplit[1].split(')')[0]);
+		}
+		const regex = new RegExp('\\/' + namePart + '\\.');
 		const soundKey = sounds.find(sound => regex.test(sound.Key)).Key;
 		const dispatcher = connection.play('https://ruediger.s3.eu-central-1.amazonaws.com/' + soundKey);
+		if(soundSplit[1]) {
+			dispatcher.on('start', () => {
+				setTimeout(() => {
+					dispatcher.end();
+				}, timePart * 1000);
+			});
+		}
 		dispatcher.on('finish', () => {
+			dispatcher.resume();
 			playNextSound(connection, soundNames, sounds);
 		});
 	}
