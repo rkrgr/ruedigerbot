da776e6eede8c2bdbf3d43d02a5c8ef20499ad67
rkrgr <r.krgr@tuta.io>
2021-07-25 10:49:12 +0200

    change start command so it defaults to scene 1

--- src/commands/startAdventureCommand.js
+++ src/commands/startAdventureCommand.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'Starts a Ruediger adventure.',
 	execute(message, args) {
 		const adventureName = args[0];
-    const numOfScene = args[1];
+    const numOfScene = args[1] ? args[1] : 1;
 
 		adventure.start(message.member.voice.channel, message.channel, adventureName, numOfScene);
 	},
