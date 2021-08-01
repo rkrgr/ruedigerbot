c29a0b7e6e0e3ae27cf8d466d2c2d045ab005e8c
rkrgr <r.krgr@tuta.io>
2021-08-01 16:30:45 +0200

    add eslint

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
+  require('dotenv').config();
 }
-
 
 const s3 = require('./src/s3database');
 
@@ -13,58 +12,56 @@ const { play } = require('./src/soundplayer');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./src/commands/${file}`);
-	client.commands.set(command.name, command);
+  client.commands.set(command.name, command);
 }
 
 const adventure = require('./src/adventure/adventureController');
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', (message) => {
   if (message.author.bot) return;
-	if (!message.content.startsWith(process.env.PREFIX)) {
-    if(adventure.adventureIsActive()) {
+  if (!message.content.startsWith(process.env.PREFIX)) {
+    if (adventure.adventureIsActive()) {
       adventure.computeInput(message.content.trim());
     }
     return;
-  };
+  }
 
-	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-	try {
-		if (client.commands.has(command)) {
-			client.commands.get(command).execute(message, args);
-		}
-		else {
-			// try to play sound if command does not exist
-			args.unshift(command);
-			client.commands.get('play').execute(message, args);
-		}
-	}
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+  try {
+    if (client.commands.has(command)) {
+      client.commands.get(command).execute(message, args);
+    } else {
+      // try to play sound if command does not exist
+      args.unshift(command);
+      client.commands.get('play').execute(message, args);
+    }
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+  }
 });
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-	const oldVoice = oldState.channelID;
-	const newVoice = newState.channelID;
+  const oldVoice = oldState.channelID;
+  const newVoice = newState.channelID;
 
-	if(oldVoice == null && newVoice != null) {
-		s3.getWelcomeSound(newState.member.user.id).then(soundName => {
-			if(soundName) {
-				play(newState.member.voice.channel, [ soundName ]);
-			}
-		});
-	}
+  if (oldVoice == null && newVoice != null) {
+    s3.getWelcomeSound(newState.member.user.id).then((soundName) => {
+      if (soundName) {
+        play(newState.member.voice.channel, [soundName]);
+      }
+    });
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- src/soundplayer.js
+++ src/soundplayer.js
@@ -1,44 +1,56 @@
 const s3 = require('./s3database');
 
 async function play(voiceChannel, soundNames, folder = 'sounds') {
-	try {
-		const playlistName = soundNames[0];
-		const playlist = await s3.getPlaylist(playlistName);
-		if(playlist) {
-			soundNames = JSON.parse(playlist);
-		}
+  try {
+    const playlistName = soundNames[0];
+    const playlist = await s3.getPlaylist(playlistName);
+    if (playlist) {
+      soundNames = JSON.parse(playlist);
+    }
 
-		const sounds = await s3.getSounds(folder);
-    
-		const legitSoundNames = soundNames.filter(soundName => {
-			const namePart = soundName.split('(')[0];
-			const regex = new RegExp('\\/' + namePart + '\\.');
-			return sounds.find(sound => regex.test(sound.Key));
-		});
+    const sounds = await s3.getSounds(folder);
 
-		const connection = await voiceChannel.join();
-		await playNextSound(connection, legitSoundNames, sounds);
-	}
-	catch(e) {
-		console.log(e);
-	}
+    const legitSoundNames = soundNames.filter((soundName) => {
+      const namePart = soundName.split('(')[0];
+      const regex = new RegExp(`\\/${namePart}\\.`);
+      return sounds.find((sound) => regex.test(sound.Key));
+    });
+
+    const connection = await voiceChannel.join();
+    await playNextSound(connection, legitSoundNames, sounds);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function playFromFile(voiceChannel, file) {
-	const connection = await voiceChannel.join();
-	return connection.play(file);
+  const connection = await voiceChannel.join();
+  return connection.play(file);
+}
+
+function getSoundName(sound) {
+  const soundSplit = sound.split('(');
+  return soundSplit[0];
+}
+
+function getSoundPlaytime(sound) {
+  const soundSplit = sound.split('(');
+  if (soundSplit[1]) {
+    return parseFloat(soundSplit[1].split(')')[0]);
+  }
+  return null;
 }
 
 function playNextSound(connection, soundNames, sounds) {
   return new Promise((resolve, reject) => {
     const nextSound = soundNames.shift();
-    if(nextSound) {
+    if (nextSound) {
       const namePart = getSoundName(nextSound);
       const timePart = getSoundPlaytime(nextSound);
-      const regex = new RegExp('\\/' + namePart + '\\.');
-      const soundKey = sounds.find(sound => regex.test(sound.Key)).Key;
-      const dispatcher = connection.play('https://ruediger.s3.eu-central-1.amazonaws.com/' + soundKey);
-      if(timePart) {
+      const regex = new RegExp(`\\/${namePart}\\.`);
+      const soundKey = sounds.find((sound) => regex.test(sound.Key)).Key;
+      const dispatcher = connection.play(`https://ruediger.s3.eu-central-1.amazonaws.com/${soundKey}`);
+      if (timePart) {
         dispatcher.on('start', () => {
           setTimeout(() => {
             dispatcher.end();
@@ -51,19 +63,6 @@ function playNextSound(connection, soundNames, sounds) {
       });
     }
   });
-}
-
-function getSoundName(sound) {
-  const soundSplit = sound.split('(');
-  return soundSplit[0];
-}
-
-function getSoundPlaytime(sound) {
-  const soundSplit = sound.split('(');
-  if(soundSplit[1]) {
-    return parseFloat(soundSplit[1].split(')')[0]);
-  }
-  return null;
 }
 
 exports.play = play;
